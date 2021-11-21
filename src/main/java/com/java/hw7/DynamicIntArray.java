61e0b9e6631c6910b127d52737379226947740e4
Zhenya2809 <Zhenya2809@ukr.net>
2021-11-21 17:54:02 +0200

    new

--- src/main/java/com/java/hw7/DynamicIntArray.java
+++ src/main/java/com/java/hw7/DynamicIntArray.java
@@ -1,64 +1,84 @@
 package com.java.hw7;
 
+import java.util.Arrays;
+
 public class DynamicIntArray {
 
     /**
      * Array for storing numbers
      */
-    private int[] array;
-
+    private int[] array = new int[10];
     /**
      * Current size of the array
      */
-    private int size;
+    private int size = 0;
 
     /**
      * Method for adding element to the array
+     *
      * @param element represents number need to add
      */
     public void add(int element) {
+        if (size == array.length) {
+            resize();
+        }
+        array[size++] = element;
     }
 
     /**
      * Method for removing first element from array
+     *
      * @param element represents number need to remove
      * @return true if successfully removed and false if the array doesn't contain such an element
      */
     public boolean remove(int element) {
-        return false;
+        if (array[0] == element) {
+            System.arraycopy(array, 1, array, 0, array.length - 1);
+            size--;
+            return true;
+        } else return false;
     }
 
     /**
      * Method for updating element by index
+     *
      * @param element represents new value of the element with appropriate index
-     * @param index represents the position of the element which need to update
+     * @param index   represents the position of the element which need to update
      * @return true if successfully updated and false if the array doesn't contain such an element
      */
     public boolean update(int element, int index) {
-        return false;
+        if (index >= size) {
+            return false;
+        } else {
+            array[index] = element;
+            return true;
+        }
     }
 
     /**
      * Method for retrieving an element by index. If the array doesn't contain an element at the position then throw NoSuchElementException
+     *
      * @param index represents the position of the element which need to retrieve
      * @return an element at the position with appropriate index
      */
     public int getElement(int index) {
-        return -1;
+        return array[index];
     }
 
     /**
      * Method for retrieving size of the array.
+     *
      * @return size of the array
      */
     public int getSize() {
-        return -1;
+        return size;
     }
 
     /**
      * Method for creating new array if previous is full and copying all element from an old array to a new
      */
     private void resize() {
+        array = Arrays.copyOf(array, size * 3 / 2 + 1);
     }
 
 }
